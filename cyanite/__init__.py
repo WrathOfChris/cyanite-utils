43fabf4a67503f863c12f49dcf2789edeaed7a72
WrathOfChris <chris@wrathofchris.com>
2014-11-11 11:13:21 -0800

    0.0.2

--- cyanite/__init__.py
+++ cyanite/__init__.py
@@ -2,7 +2,7 @@
 Tools for managing a Cyanite cluster.
 """
 
-__version_info__ = ('0', '0', '1')
+__version_info__ = ('0', '0', '2')
 __version__ = '.'.join(__version_info__)
 
 class CyaniteException(Exception):
